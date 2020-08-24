4dcdaa2e33aff37433118cee7eba12298eabaccd
jp-96 <jp-96@outlook.jp>
2020-08-24 14:28:44 +0900

    bundle gem jekyll-scratchblocks -t

---
+++ lib/jekyll/scratchblocks.rb
@@ -0,0 +1,8 @@
+require "jekyll/scratchblocks/version"
+
+module Jekyll
+  module Scratchblocks
+    class Error < StandardError; end
+    # Your code goes here...
+  end
+end
---
+++ lib/jekyll/scratchblocks/version.rb
@@ -0,0 +1,5 @@
+module Jekyll
+  module Scratchblocks
+    VERSION = "0.1.0"
+  end
+end
